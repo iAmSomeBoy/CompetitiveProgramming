bad0f6e4c7f44f22c2be2befaeb0e32791b26013
Md Ashikur Rahman <ashikur2401@gmail.com>
2025-01-05 23:03:40 +0600

    Add files via upload

---
+++ UnSolved_problems_at_cf/King Keykhosrows Mystery.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main()
+{
+    int t ,a,b ;
+    scanf("%d",&t);
+
+    while(t--)
+    {
+        scanf("%d %d",&a,&b);
+
+        for(int i=a ;i<=1000;i++)
+        {
+           
+            if(i%a == i%b)
+            {   
+                printf("%d ",i);
+                
+            }
+            break ;
+            
+            if(m!=n)
+            continue ;
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+    return(0);
+}
