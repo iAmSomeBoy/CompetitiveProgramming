6bb03f2e0acbd0d3e6484220bb69f17be9e83863
iAmSomeBoy <ashikur2401@gmail.com>
2026-01-06 14:00:15 +0600

    ..

---
+++ A_Four_Digits.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int N;
+    cin>>N;
+
+    string s = to_string(N);
+
+    cout<<setw(4)<<setfill('0')<<s<<endl;
+
+    return 0;
+}
